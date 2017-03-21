787c67e58318a9182ce097a39bba65fe8f87b676
Sourec <crmyers@wpi.edu>
2017-03-20 20:28:14 -0400

    Implement ASM based GBP solution

--- bits.c
+++ bits.c
@@ -171,12 +171,12 @@ NOTES:
  *   Max ops: 8
  *   Rating: 2
  */
-/*char* bytestr(int byte) {
+char* bytestr(int byte) {
 	char *str = malloc(32); //hey look, a memory leak!
 	for (char i = 0; i < 32; i++)
 		str[31 - i] = (1 << i) & byte ? '1' : '0';
 	return str;
-}*/
+}
 
 int oddBits(void) {
 	//Must OR together a lot of smaller constants because constants bigger than 8 bits are forbidden
@@ -231,9 +231,12 @@ int conditional(int x, int y, int z) {
  *   Rating: 4 
  */
 int greatestBitPos(int x) {
-	int mask = 0;
-
-	return mask;
+	int r = x;
+	asm("bsr %1, %0\n"
+	    :"=r" (r)
+	    :"r" (x)
+	    :"%eax");
+	return 1<<r & (((!!x)<<31)>>31);
 }
 
 /* 
@@ -279,7 +282,7 @@ int isNonNegative(int x) {
  *   Rating: 3
  */
 int satMul2(int x) {
-	return 2;
+	return x << 1;
 }
 
 /* 
