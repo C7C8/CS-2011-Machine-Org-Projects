987ccdd5776bb389764b9f87f5fc1c8dc352a256
Sourec <crmyers@wpi.edu>
2017-04-24 13:57:09 -0400

    Final prep on cache simulator

--- csim.c
+++ csim.c
@@ -1,22 +1,37 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "cachelab.h"
 #include "cmdline.h"
+
+const int INPUT_BUF_SIZE = 1024;
 
 int main(int argc, char** argv)
 {
     struct gengetopt_args_info args;
 	cmdline_parser(argc, argv, &args);
 
-	printf("Set index bits: %d\n", args.associativity_arg);
-    printf("Associativity: %d\n", args.associativity_arg);
-    printf("Block bits: %d\n", args.block_bits_arg);
-	if (!args.tracefile_given) {
-		if (args.verbose_given)
-			printf("Using stdin instead of tracefile!\n");
+	if (args.verbose_given) {
+		printf("Set index bits: %d\n", args.associativity_arg);
+		printf("Associativity: %d\n", args.associativity_arg);
+		printf("Block bits: %d\n", args.block_bits_arg);
+		if (!args.tracefile_given)
+			printf("No tracefile provided, using stdin instead\n");
+		else
+			printf("Tracefile: %s\n", args.tracefile_arg);
 	}
-	else
-    	printf("Tracefile: %s\n", args.tracefile_arg);
-    printf("Verbose? %d\n", args.verbose_given);
-    printSummary(0, 0, 0);
+
+	FILE* tracefile = stdin;
+	if (args.tracefile_given && (tracefile = fopen(args.tracefile_arg, "r")) == NULL){
+		perror("Couldn't access tracefile");
+		abort();
+	}
+
+	//Now that the setup work is all done, let's do some some cache simulation work
+	int cacheHits = 0, cacheMisses = 1, cacheEvictions = 2;
+	char inputBuf[INPUT_BUF_SIZE];
+
+
+    printSummary(cacheHits, cacheMisses, cacheEvictions);
     return 0;
 }
