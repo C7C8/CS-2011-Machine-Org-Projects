e1f5f62d8b121ba8b0d5d02d361831588cfa7787
Sourec <crmyers@wpi.edu>
2017-04-29 17:39:31 -0400

    Remove unneeded files, improve performance thingy

--- trans.c
+++ trans.c
@@ -23,23 +23,33 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-	register int i, j;
-
 	//This isn't code I used when writing this (that would be the stupidest thing I've ever done, and that's saying something!).
 	//It's what came to mind when I wrote this.
 	//https://gist.githubusercontent.com/alessonforposterity/832da4fab11e10609dad/raw/258df12378399919ae088ba8731a7571d9c2c947/drgn.txt
-	register int a0, a1, a2, a3;
+	register int i, j, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
 
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < M; j += 4) {
+	for (j = 0; j < M; j += 8) {
+		for (i = 0; i < N; i++) {
 			a0 = A[i][j];
 			a1 = A[i][j+1];
 			a2 = A[i][j+2];
 			a3 = A[i][j+3];
+			a4 = A[i][j+4];
+			a5 = A[i][j+5];
+			a6 = A[i][j+6];
+			a7 = A[i][j+7];
+			a8 = A[i][j+8];
+			a9 = A[i][j+9];
 			B[j][i] = a0;
 			B[j+1][i] = a1;
 			B[j+2][i] = a2;
 			B[j+3][i] = a3;
+			B[j+4][i] = a4;
+			B[j+5][i] = a5;
+			B[j+6][i] = a6;
+			B[j+7][i] = a7;
+			B[j+8][i] = a8;
+			B[j+9][i] = a9;
 		}
 	}
 }
