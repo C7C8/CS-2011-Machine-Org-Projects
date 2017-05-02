a773913d60a02fef0d93cbfb3f2d66538e775ed1
Sourec <crmyers@wpi.edu>
2017-05-02 15:16:09 -0400

    Add name to trans.c

--- trans.c
+++ trans.c
@@ -1,12 +1,4 @@
-/* 
- * trans.c - Matrix transpose B = A^T
- *
- * Each transpose function must have a prototype of the form:
- * void trans(int M, int N, int A[N][M], int B[M][N]);
- *
- * A transpose function is evaluated by counting the number of misses
- * on a 1KB direct mapped cache with a block size of 32 bytes.
- */
+//crmyers (Christopher Myers) 2017
 #include "cachelab.h"
 
 char rectanglemode_desc[] = "Rectangle blockmode";
