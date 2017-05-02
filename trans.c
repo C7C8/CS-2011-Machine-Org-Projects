ce980327c790e52626c8044007d31f4de735dc36
Sourec <crmyers@wpi.edu>
2017-05-02 15:05:27 -0400

    Magically get a bunch more points

--- trans.c
+++ trans.c
@@ -12,44 +12,6 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-	//This link isn't code I used when writing this (that would be the stupidest thing I've ever done, and that's saying something!).
-	//It's what came to mind when I wrote this.
-	//https://gist.githubusercontent.com/alessonforposterity/832da4fab11e10609dad/raw/258df12378399919ae088ba8731a7571d9c2c947/drgn.txt
-	int i, j, a0, a1, a2, a3, a4, a5, a6, a7;
-
-	for (j = 0; j < M;) {
-		for (i = 0; i < N; i++) {
-			a0 = A[i][j];
-			a1 = A[i][j+1];
-			a2 = A[i][j+2];
-			a3 = A[i][j+3];
-			if (M == 32){
-				a4 = A[i][j+4];
-				a5 = A[i][j+5];
-				a6 = A[i][j+6];
-				a7 = A[i][j+7];
-			}
-			B[j][i] = a0;
-			B[j+1][i] = a1;
-			B[j+2][i] = a2;
-			B[j+3][i] = a3;
-			if (M == 32) {
-				B[j+4][i] = a4;
-				B[j+5][i] = a5;
-				B[j+6][i] = a6;
-				B[j+7][i] = a7;
-			}
-		}
-		if (M == 32)
-			j += 8;
-		else
-			j += 4;
-	}
-}
-
 char rectanglemode_desc[] = "Rectangle blockmode";
 void rectanglemode(int M, int N, int A[N][M], int B[M][N]) {
 	int i, j, blockI, tI, tI2;
@@ -88,6 +50,40 @@ void rectanglemode(int M, int N, int A[N][M], int B[M][N]) {
 	}
 
 }
+
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+	//This link isn't code I used when writing this (that would be the stupidest thing I've ever done, and that's saying something!).
+	//It's what came to mind when I wrote this.
+	//https://gist.githubusercontent.com/alessonforposterity/832da4fab11e10609dad/raw/258df12378399919ae088ba8731a7571d9c2c947/drgn.txt
+	int i, j, a0, a1, a2, a3, a4, a5, a6, a7;
+	if (M == 64 && N == 64)
+		return rectanglemode(M, N, A, B);
+
+	for (j = 0; j < M;) {
+		for (i = 0; i < N; i++) {
+			a0 = A[i][j];
+			a1 = A[i][j+1];
+			a2 = A[i][j+2];
+			a3 = A[i][j+3];
+			a4 = A[i][j+4];
+			a5 = A[i][j+5];
+			a6 = A[i][j+6];
+			a7 = A[i][j+7];
+			B[j][i] = a0;
+			B[j+1][i] = a1;
+			B[j+2][i] = a2;
+			B[j+3][i] = a3;
+			B[j+4][i] = a4;
+			B[j+5][i] = a5;
+			B[j+6][i] = a6;
+			B[j+7][i] = a7;
+		}
+		j += 8;
+	}
+}
+
 void registerFunctions()
 {
 	registerTransFunction(transpose_submit, transpose_submit_desc);
